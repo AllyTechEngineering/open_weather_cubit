bfcae2cd77776d796bcac0874a430436a24b9edf
Admin <btaylor@allytechllc.com>
2023-10-17 11:16:13 -0400

    first commit

---
+++ lib/models/direct_geocoding.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class DirectGeocoding extends Equatable {
+  final String name;
+  final double lat;
+  final double lon;
+  final String country;
+  DirectGeocoding({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.country,
+  });
+
+  /// factory constructor
+  factory DirectGeocoding.fromJson(List<dynamic> json) {
+    final Map<String, dynamic> data = json[0];
+    return DirectGeocoding(
+        name: data['name'], lat: data['lat'], lon: data['lon'], country: data['country']);
+  } //factory DirectGeocoding
+
+  @override
+  List<Object> get props => [name, lat, lon, country];
+
+  @override
+  String toString() {
+    return 'DirectGeocoding(name: $name, lat: $lat, lon: $lon, country: $country)';
+  }
+} //class DirectGeocoding
